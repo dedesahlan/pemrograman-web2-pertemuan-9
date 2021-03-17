7a61b3c7ea56d7919a59ef1ba41cc58fbd4980cd
dedesahlan <79922951+dedesahlan@users.noreply.github.com>
2021-03-17 23:04:41 +0700

    Add files via upload

---
+++ pert9_latihan4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanggal</title>
+	<style type="text/css">
+		h3	{
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+	
+	<?php 
+	echo "Sekarang Tanggal";
+	echo date('d-F-Y');
+	echo "<br> dan jam";
+	echo date('h:i:s A');
+	 ?>
+	</font>
+</body>
+</html>
